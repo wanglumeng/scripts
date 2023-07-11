19aa02bc9afa18cecd673c0c3e5021df04a4bfc3
wanglumeng <wanglumeng@trunk.tech>
2023-07-11 15:45:03 +0800

    trunk_common

---
+++ source_trunk_common.zsh
@@ -0,0 +1,6 @@
+LD_LIBRARY_PATH=/opt/ros/melodic/lib:/opt/trunk_master/lib
+CMAKE_PREFIX_PATH=/opt/ros/melodic:/opt/trunk_master
+PKG_CONFIG_PATH=/opt/ros/melodic/lib/pkgconfig:/opt/trunk_master/lib/pkgconfig:/opt/trunk_master/share/pkgconfig:/opt/trunk_master/lib64/pkgconfig
+
+export LD_LIBRARY_PATH CMAKE_PREFIX_PATH PKG_CONFIG_PATH
+
