846c66cf6bdf4ae8182d6e8f10176839b5535cd4
wanglumeng <wanglumeng@trunk.tech>
2023-12-21 14:38:18 +0800

    source trunk common

--- source_trunk_common.zsh
+++ source_trunk_common.zsh
@@ -1,3 +1,4 @@
+# trunk_common default install path: /opt/trunk_master/
 export LD_LIBRARY_PATH=/opt/trunk_master/lib:${LD_LIBRARY_PATH}
 export CMAKE_PREFIX_PATH=/opt/trunk_master:${CMAKE_PREFIX_PATH}
 export PKG_CONFIG_PATH=/opt/trunk_master/lib/pkgconfig:/opt/trunk_master/share/pkgconfig:${PKG_CONFIG_PATH}
